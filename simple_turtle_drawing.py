f1091bdd11d48942d77a1c630c5034e493b429ec
Anatasia <114031710+Anatasia1987@users.noreply.github.com>
2022-11-28 15:16:31 +1300

    Add files via upload

---
+++ simple_turtle_drawing.py
@@ -0,0 +1,25 @@
+
+from turtle import *
+
+pensize (20)
+
+
+l = 200
+w = 300
+
+# drawing first side
+forward(w)
+left(90)    
+
+# drawing second side
+forward(l)
+left(90)
+
+# drawing third side
+forward(w)
+left(90)
+
+# drawing fourth side
+forward(l)
+left(90)
+
