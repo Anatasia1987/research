d0bbe57438273fbe556b8c21cac11972d9550487
Anatasia <114031710+Anatasia1987@users.noreply.github.com>
2022-12-02 22:05:18 +1300

    Add files via upload

---
+++ anatasia.py
@@ -0,0 +1,159 @@
+import turtle
+anatasia = turtle.Turtle()
+
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.goto(-300,200)
+anatasia.color ("pink")
+
+anatasia.pendown()
+anatasia.forward (55)
+anatasia.right (45)
+anatasia.forward (65)
+anatasia.backward (100)
+
+anatasia.right (90)
+anatasia.forward (90)
+
+anatasia.penup()
+
+
+#end of letter A
+
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.pendown()
+anatasia.pensize (10)
+anatasia.pencolor ("blue")
+
+anatasia.right (75)
+anatasia.forward (100)
+anatasia.backward (120)
+
+anatasia.left (40)
+anatasia.forward (155)
+
+anatasia.left (139)
+anatasia.forward (120)
+
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.goto(-300,200)
+anatasia.color ("pink")
+
+# end of letter N
+
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.goto(-300,200)
+anatasia.color ("pink")
+
+anatasia.pendown()
+anatasia.forward (55)
+anatasia.right (45)
+anatasia.forward (65)
+anatasia.backward (100)
+
+anatasia.right (90)
+anatasia.forward (90)
+
+#end of letter A
+#start letter T
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.goto(-300,200)
+anatasia.color ("pink")
+
+anatasia.hideturtle()
+anatasia.color("red")
+anatasia.speed(5)
+anatasia.pensize(8)
+anatasia.penup()
+anatasia.setpos(-175,75)
+anatasia.pendown()
+anatasia.speed(1)
+anatasia.setpos(-125,75)
+anatasia.speed(8)
+anatasia.setpos(-150,75)
+anatasia.speed(1)
+anatasia.setpos(-150,10)
+
+#end of letter T
+#start letter A
+
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.goto(-300,200)
+anatasia.color ("pink")
+
+anatasia.pendown()
+anatasia.forward (55)
+anatasia.right (45)
+anatasia.forward (65)
+anatasia.backward (100)
+
+anatasia.right (90)
+anatasia.forward (90)
+
+anatasia.penup()
+#end of letter A
+
+#start letter S
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+
+anatasia.goto(-300,200)
+anatasia.color ("blue")
+anatasia.pendown()
+anatasia.forward (30)
+anatasia.backward (30)
+
+anatasia.circle (-90, -185)
+anatasia.circle (90, -250)
+
+#end of letter S
+#start letter I
+
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.goto(-300,200)
+anatasia.color ("pink")
+
+anatasia.pendown()
+anatasia.forward (100)
+anatasia.backward (200)
+anatasia.forward (100)
+anatasia.right (90)
+anatasia.forward (150)
+anatasia.left (90)
+anatasia.forward (100)
+anatasia.backward (200)
+
+#end of letter I
+
+#start of letter A
+
+anatasia.reset()
+anatasia.pensize(5)
+anatasia.penup()
+anatasia.goto(-300,200)
+anatasia.color ("pink")
+
+anatasia.pendown()
+anatasia.forward (55)
+anatasia.right (45)
+anatasia.forward (65)
+anatasia.backward (100)
+
+anatasia.right (90)
+anatasia.forward (90)
+
+anatasia.penup()
+#end of letter A
