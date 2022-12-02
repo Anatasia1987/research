64b0f5a758068cc89e8f2553a99a09cd3a64eadc
Anatasia <114031710+Anatasia1987@users.noreply.github.com>
2022-12-02 21:00:17 +1300

    Add files via upload

---
+++ MAINticket.py
@@ -0,0 +1,13 @@
+#this is ticket lists
+
+#Ticket number, creator name, staff ID, email address, description of the issue, response from IT Department
+
+main_ticket = "ticket number, creator name, staff ID, email address, description, response from IT department"
+
+print(main_ticket)
+
+#python list
+
+ticket_list =["ticket number, creator name, staff ID, email address, description, response from IT department"]
+
+print (type (ticket_list))
